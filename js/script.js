cc33e5f52b2bb671bf0b799c4fa60193e3fba153
Agustin <aguu73470@gmail.com>
2023-09-12 19:24:20 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// alert("Hola este es mi Javascript");
+let nombre= "Agustin";
+nombre = "2"
+
+var nombre1= "Daniela";
+
+const nombre2= "Rocio";
+
+console.log(nombre)
+console.log(nombre1)
+console.log(nombre2)
